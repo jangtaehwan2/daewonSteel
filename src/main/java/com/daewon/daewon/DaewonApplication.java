658deaa437c2c2696c5fe4b98e8e2ee0364acb49
Buddy <jangtaehwan2@gmail.com>
2022-02-23 23:44:05 +0900

    create project

---
+++ src/main/java/com/daewon/daewon/DaewonApplication.java
@@ -0,0 +1,13 @@
+package com.daewon.daewon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaewonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaewonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daewon/daewon/DaewonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daewon.daewon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaewonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
